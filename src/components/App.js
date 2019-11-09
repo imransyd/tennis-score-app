7f6a299856f2c1af6aa0662ec47d9c3173c15aa6
imransyd <imransyd.88@gmail.com>
2019-11-09 23:43:42 +0100

    initiating a webpack react app

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Tennis Score</h1>
+            </div>
+        )
+    }
+}
